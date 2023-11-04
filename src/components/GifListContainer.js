f99e1e97b2bc111da98c6e49b9bb08df597d62f2
Everlyn <gachuieve32@gmail.com>
2023-11-04 12:38:04 +0300

    completed

--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -17,24 +17,7 @@ function GifListContainer() {
       .catch(error=>console.log(error))
     },[search])
 console.log(gifData)
-    // function handleSearch(search){
-    //   if(search.length > 0){
-    //     const searchUrl = "https://api.giphy.com/v1/gifs/search?api_key=UYgEyqrGdTZBv2tFNbvN2EGIUqWXXNpV&rating=g&limit=3&q="+search
-    //   fetch(searchUrl)
-    //   .then(response => response.json())
-    //   .then(data => {
-    //     setGifData(data);
-    //     // const results = data.filter((item)=>{
-    //     //   return (search&& item&& item.title.toLowerCase().includes(search))
-    //     // })
-    //     // console.log(results)
-    //   })
-
-    //   }
-      
-    // }
-
-// console.log(gifData.data)
+   
   return (
     <div style={{display:'flex',justifyContent:'space-around'}}>
       <GifList data={gifData}/>
@@ -49,34 +32,3 @@ export default GifListContainer
 
 
 
-// import React,{ useState,useEffect} from 'react'
-// import GifList from './GifList'
-// import GifSearch from './GifSearch'
-// const GifListContainer = () => {
-
-//     const[gif, setGif] = useState([])
-//     const [search, setSearch] = useState("dolphins");
-    
-//     const apiKey='1iwYJT1yONNHCIeXGdp7ZHi5KcGBZKYx';
-
-//     useEffect(()=>{
-//         fetch(`https://api.giphy.com/v1/gifs/search?q=${search}&api_key=${apiKey}&rating=g`)
-//         .then((res)=>res.json())
-//         .then(({data})=>{
-//             const gifs = data.slice(0,3).map((gif) => ({ url: gif.images.original.url }));
-//             setGif(gifs);
-            
-//         })
-    
-//     },[search])
-
-
-//   return (
-//     <div style={{display:'flex',justifyContent:'space-around'}}>
-//      <GifList  gifs={gif} />
-//       <GifSearch onSubmitQuery={setSearch} />
-//     </div>
-//   )
-// }
-
-// export default GifListContainer
